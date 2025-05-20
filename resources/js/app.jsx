829a42049dd578c884ab1cc62e77b4d87e19f0a4
DelPro123 <siribanjohndel@gmail.com>
2025-05-20 12:24:06 +0800

    change design

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,8 @@ import GamesList from './Components/GamesList'
 import ColorGame from './Components/ColorGame'
 import PredictionHistory from './Components/PredictionHistory'
 import CountdownPage from './Components/CountdownPage'
+import { Helmet } from 'react-helmet'
+import '../css/app.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,6 +13,11 @@ ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
 
     <div className='container-fluid'>
+        <Helmet>
+            <meta charSet="utf-8" />
+                <title>My Title</title>
+                <link rel="canonical" href=" " />
+        </Helmet>
         <div className='row'>
             <CountdownPage/>
         </div>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,12 @@
+<!-- resources/views/app.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>React in Laravel</title>
+    <meta charset="utf-8">
+    <title>Prediction Game</title>
+
     @viteReactRefresh
-    @vite(['resources/js/app.jsx'])
+    @vite('resources/js/app.jsx')
 </head>
 <body>
     <div id="app"></div>
