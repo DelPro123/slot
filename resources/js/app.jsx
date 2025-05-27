478435133610c76750821ccbb3d99db11ec185ef
DelPro123 <siribanjohndel@gmail.com>
2025-05-27 15:26:50 +0800

    changes

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,6 @@ import GamesList from './Components/GamesList'
 import ColorGame from './Components/ColorGame'
 import PredictionHistory from './Components/PredictionHistory'
 import CountdownPage from './Components/CountdownPage'
-import { Helmet } from 'react-helmet'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/app.scss';
 
@@ -13,12 +12,6 @@ ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
 
     <div className='container-fluid'>
-        <Helmet>
-            <meta charSet="utf-8" />
-                 <title>Chinchin Casino Slot Prediction</title>
-                <meta name="description" content="Chinchin Casino Slot Prediction, winning color-based winning rate chance with real-time updates and history tracking." />
-                <link rel="icon" href="https://chinchin-casino.com/wp-content/uploads/2025/04/Chinchin-Favicon.webp" />
-        </Helmet>
         <div className='row'>
             <CountdownPage/>
         </div>
